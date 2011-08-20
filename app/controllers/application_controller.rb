c1f486452982a5f3f2d572e0d308bcace684d084
james delacruz <james@kyokushin.com.ph>
2011-08-20 12:11:30 +0800

    added photo james the finisher touch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "James Martin Dela Cruz"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
