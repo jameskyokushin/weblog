f4362f0fac60659c0f231684f976474a8e376bdd
james delacruz <james@kyokushin.com.ph>
2011-08-25 18:24:22 +0800

    search ok rank/dojo/name

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ActiveRecord::Base
   
   def self.search(search)
     if search
-      find(:all, :conditions => ['full_name LIKE ?', "%#{search}%"]) 
+      find(:all, :conditions => ['full_name like :key or dojo like :key or rank like :key',:key => "%#{search}%"]) 
     else
       find(:all)
     end
